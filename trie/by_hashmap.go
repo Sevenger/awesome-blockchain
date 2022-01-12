8639649c521fce77cfb7460ff9c54d4b49b88f95
lilongjie <arcfya@gmail.com>
2022-01-12 13:03:25 +0800

    clean code

--- trie/by_hashmap.go
+++ trie/by_hashmap.go
@@ -1,1 +1,43 @@
 package trie
+
+type MapTreeNode struct {
+	Child  map[rune]*MapTreeNode
+	IsWord bool
+}
+
+func NewMapTrieTree() *MapTreeNode {
+	return &MapTreeNode{
+		Child: make(map[rune]*MapTreeNode),
+	}
+}
+
+func (root *MapTreeNode) Insert(word string) {
+	cur := root
+	for _, s := range word {
+		if _, ok := cur.Child[s]; !ok {
+			cur.Child[s] = NewMapTrieTree()
+		}
+		cur = cur.Child[s]
+	}
+	cur.IsWord = true
+}
+
+func (root *MapTreeNode) Search(word string) bool {
+	tail := root.getTailNode(word)
+	return tail != nil && tail.IsWord
+}
+
+func (root *MapTreeNode) StartWith(prefix string) bool {
+	return root.getTailNode(prefix) != nil
+}
+
+func (root *MapTreeNode) getTailNode(word string) *MapTreeNode {
+	cur := root
+	for _, s := range word {
+		if _, ok := cur.Child[s]; !ok {
+			return nil
+		}
+		cur = cur.Child[s]
+	}
+	return cur
+}
