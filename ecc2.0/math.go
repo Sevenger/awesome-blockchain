0a4d5b2bc75aa8f23a222b1390c44bc00fa237d6
lilongjie <arcfya@gmail.com>
2022-01-14 13:24:14 +0800

    ecc

--- ecc2.0/math.go
+++ ecc2.0/math.go
@@ -7,6 +7,8 @@ import (
 var convertBigIntErr = "convert %s to big.Int failed"
 
 var Zero = new(big.Int).SetInt64(0)
+var One = new(big.Int).SetInt64(1)
+var Two = new(big.Int).SetInt64(2)
 
 func IsEqual(x, y *big.Int) bool {
 	return x.Cmp(y) == 0
@@ -16,92 +18,29 @@ func IsEqual(x, y *big.Int) bool {
 func RatMod(rat *big.Rat, p *big.Int) *big.Int {
 	n := rat.Num()
 	d := rat.Denom()
-
-	type powFunc func(num, n *big.Int) *big.Int
-	var fastPow powFunc
-	var one = big.NewInt(1)
-	var two = big.NewInt(2)
-
-	fastPow = func(num, n *big.Int) *big.Int {
-		if IsEqual(n, one) {
-			return big.NewInt(1)
-		} else if IsEqual(Mod(n, two), one) {
-			return Mod(Mul(fastPow(num, Sub(n, one)), num), p)
-		} else {
-			temp := Mod(fastPow(num, Div(n, two)), p)
-			return Mod(Mul(temp, temp), p)
-		}
-	}
-
-	fastPow = func(a, n *big.Int) *big.Int {
-		res := big.NewInt(1)
-		for i := big.NewInt(0); i.Cmp(n) < 0; i.Add(i, one) {
-			res.Mul(res, a)
-		}
-		return res.Mod(res, p)
-	}
-
-	fastPow = func(a1, n *big.Int) *big.Int {
-		res := big.NewInt(1)
-		a := new(big.Int).Set(a1)
-		a.Mod(a, p)
-		for !IsEqual(n, Zero) {
-			if IsEqual(new(big.Int).And(n, one), one) {
-				res.Mul(res, a).Mod(res, p)
-			}
-			a.Mul(a, a).Mod(a, p)
-			n.Rsh(n, 1)
-		}
-		return res
+	if IsEqual(d, One) {
+		return new(big.Int).Mod(n, p)
 	}
 
 	// ( n·d^(p-2) ) % p
-	res := fastPow(d, Sub(p, big.NewInt(2)))
-
-	res.Mul(res, Mod(n, p))
+	res := FastPow(d, new(big.Int).Sub(p, Two), p)
+	res.Mul(res, n)
 	res.Mod(res, p)
 	return res
 }
 
-func ReliableRatMod(rat *big.Rat, p *big.Int) *big.Int {
-	if rat.Denom().Int64() == 1 {
-		return new(big.Int).Mod(rat.Num(), p)
+func FastPow(a, n, p *big.Int) *big.Int {
+	if IsEqual(n, Zero) {
+		return big.NewInt(1)
+	} else if IsEqual(new(big.Int).Mod(n, Two), One) {
+		//	奇数 a^n == a^(n-1) * a
+		res := FastPow(a, new(big.Int).Sub(n, One), p)
+		res.Mod(res, p)
+		return new(big.Int).Mul(res, a)
+	} else {
+		//	偶数 a^n == a^(n/2) * a^(n/2)
+		res := FastPow(a, new(big.Int).Div(n, Two), p)
+		res.Mod(res, p)
+		return new(big.Int).Mul(res, res)
 	}
-	res := big.NewInt(1)
-	fastPow := ReliableFastPow(rat.Denom(), new(big.Int).Sub(p, big.NewInt(2)), p)
-	res.Mod(rat.Num(), p).Mul(res, fastPow).Mod(res, p)
-	return res
-}
-
-func ReliableFastPow(a, b, p *big.Int) *big.Int {
-	var res = big.NewInt(1)
-	a.Mod(a, p)
-	for b.Int64() != 0 {
-		if b.Int64()&1 == 1 {
-			res.Mul(res, a).Mod(res, p)
-		}
-		b = b.Rsh(b, 1)
-		a.Mul(a, a).Mod(a, p)
-	}
-	return res
-}
-
-func Mul(x, y *big.Int) *big.Int {
-	return new(big.Int).Mul(x, y)
-}
-
-func Div(x, y *big.Int) *big.Int {
-	return new(big.Int).Div(x, y)
-}
-
-func Add(x, y *big.Int) *big.Int {
-	return new(big.Int).Add(x, y)
-}
-
-func Sub(x, y *big.Int) *big.Int {
-	return new(big.Int).Sub(x, y)
-}
-
-func Mod(x, y *big.Int) *big.Int {
-	return new(big.Int).Mod(x, y)
 }
